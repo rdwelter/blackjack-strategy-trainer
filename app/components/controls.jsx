49c2d8aa01171aa43f9c68815bc444f4f26afcce
Ryan Welter <rdwelter@ucsc.edu>
2024-08-11 19:13:57 -0700

    added controls ui and basic hand

--- app/components/controls.jsx
+++ app/components/controls.jsx
@@ -1,1 +1,13 @@
-// placeholder
+import Button from './button';
+
+export default function Controls({ newHandFunc, hitFunc, standFunc, splitFunc, doubleFunc }) {
+    return (
+        <div className='flex justify-center p-2 space-x-2'>
+            <Button text='New Hand' onClick={newHandFunc} color='blue' />
+            <Button text='Stand' onClick={standFunc} color='green' />
+            <Button text='Split' onClick={splitFunc} color='yellow' />
+            <Button text='Double' onClick={doubleFunc} color='yellow' />
+            <Button text='Hit' onClick={hitFunc} color='red' />
+        </div>
+    );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,18 @@
 import Card from './components/card';
+import Controls from './components/controls';
 
 export default function Page() {
 	return (
 		<div id='container' className='bg-[#487860] h-screen'>
 			<div id='dealer-hand' className='flex justify-center align-center p-2'>
-				<Card id='diamonds_queen' />
+				<Card cardNum={0} showBack={false} />
+				<Card cardNum={1} showBack={true} />
 			</div>
 			<div id='player-hand' className='flex justify-center align-center p-2'>
-				<Card id='diamonds_king' />
-				<Card id='diamonds_ace' />
+				<Card cardNum={2} showBack={false} />
+				<Card cardNum={3} showBack={false} />
 			</div>
+			<Controls />
 		</div>
 	);
 }
