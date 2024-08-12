33fc479756e4137474f3827a942ddfbb32d63b7b
Ryan Welter <rdwelter@ucsc.edu>
2024-08-11 21:21:37 -0700

    new hand button functional

--- app/page.jsx
+++ app/page.jsx
@@ -7,13 +7,29 @@ import { useState } from 'react';
 export default function Page() {
 	const [dealerCards, setDealerCards] = useState([0, 1]);
 	const [playerCards, setPlayerCards] = useState([2, 3]);
-	const [gamePhase, setGamePhase] = useState('Player');
+	const [gamePhase, setGamePhase] = useState('START');
+
+	function dealNewHand() {
+		let newHand = [];
+		while (newHand.length < 4) {
+			let newCard = Math.floor(Math.random() * 52);
+			while (newHand.includes(newCard)) {
+				newCard = Math.floor(Math.random() * 52);
+			}
+			newHand.push(newCard);
+		}
+		const dealerHand = newHand.slice(0, 2);
+		const playerHand = newHand.slice(2, 4);
+		setDealerCards(dealerHand);
+		setPlayerCards(playerHand);
+		setGamePhase('Player');
+	}
 
 	return (
 		<div id='container' className='bg-[#487860] h-screen'>
 			<Hand currHand={dealerCards} isDealer={true} gamePhase={gamePhase} />
 			<Hand currHand={playerCards} isDealer={false} gamePhase={gamePhase} />
-			<Controls />
+			<Controls newHandFunc={dealNewHand} />
 		</div>
 	);
 }
