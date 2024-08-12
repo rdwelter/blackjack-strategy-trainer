74a198adb08eaaa8c8b4c72e098b49800fc4b5e2
Ryan Welter <rdwelter@ucsc.edu>
2024-08-11 20:13:38 -0700

    added hand component, copied template layout with hand

--- app/components/hand.jsx
+++ app/components/hand.jsx
@@ -1,1 +1,11 @@
-// placeholder
+import Card from './card.jsx';
+
+export default function Hand({ currHand, isDealer, gamePhase }) {
+    const currCards = currHand.map((currCard, ind) => 
+        <li key={currCard + ', ' + ind}>
+            <Card cardNum={currCard} showBack={isDealer && ind === 1 && gamePhase === 'Player'} />
+        </li>
+    );
+
+    return <ul className='flex justify-center align-center p-2'>{currCards}</ul>;
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,18 @@
-import Card from './components/card';
+'use client';
+
+import Hand from './components/hand';
 import Controls from './components/controls';
+import { useState } from 'react';
 
 export default function Page() {
+	const [dealerCards, setDealerCards] = useState([0, 1]);
+	const [playerCards, setPlayerCards] = useState([2, 3]);
+	const [gamePhase, setGamePhase] = useState('Player');
+
 	return (
 		<div id='container' className='bg-[#487860] h-screen'>
-			<div id='dealer-hand' className='flex justify-center align-center p-2'>
-				<Card cardNum={0} showBack={false} />
-				<Card cardNum={1} showBack={true} />
-			</div>
-			<div id='player-hand' className='flex justify-center align-center p-2'>
-				<Card cardNum={2} showBack={false} />
-				<Card cardNum={3} showBack={false} />
-			</div>
+			<Hand currHand={dealerCards} isDealer={true} gamePhase={gamePhase} />
+			<Hand currHand={playerCards} isDealer={false} gamePhase={gamePhase} />
 			<Controls />
 		</div>
 	);
